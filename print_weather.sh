b856ffbc0444d882edc63c8020772e5a1768b6c7
Sam Komarov <sam@skomarov.com>
2023-06-03 17:41:56 +0300

    remove colors so that the file is printed properly

--- print_weather.sh
+++ print_weather.sh
@@ -2,15 +2,14 @@
 
 if [[ $1 == "-h" ]] || [[ $1 == "--help" ]] 
 then 
-	echo "
+	echo "\
 print_weather.sh - prints current weather forecast to the default printer using lpr. 
 Usage: ./print_weather.sh [-t] [<location>]
 
 if <location> is omitted, it is determined from the IP address. 
 <location> can be any description of the desired location: City name, coordinates, place name. 
 
--t, --terminal		does not send the document to the printer - outputs the forecast directly to stdout
-"
+-t, --terminal		does not send the document to the printer - outputs the forecast directly to stdout"
 	exit 0
 fi 
 
@@ -22,7 +21,7 @@ fi
 
 location=`jq -R -r @uri <<< "$@"` # urlencode the passed in location (or leave it empty)
 
-weather=`curl -s "ru.wttr.in/$location?Fn"`
+weather=`curl -s "ru.wttr.in/$location?FnT"`
 if [[ $only_terminal ]] 
 then
 	echo "$weather"
