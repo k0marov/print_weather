79881b2e90a283125c0e8b93e6ddd114d923cd6f
Sam Komarov <sam@skomarov.com>
2023-06-03 18:39:51 +0300

    add docs about the -h arg

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # print_weather.sh # 
-
+ 
 ## About ## 
 
 print\_weather.sh - prints current weather forecast to the default printer using lpr. 
 
 ## Usage ## 
 
-Usage: ./print\_weather.sh \[-t] [\<location>]
+Usage: ./print\_weather.sh \[-h] \[-t] [\<location>]
 
 ### Arguments ### 
 
@@ -15,6 +15,7 @@ if \<location> is omitted, it is determined from the IP address.
 \<location> can be any description of the desired location: City name, coordinates, place name. 
 
 -t, --terminal		does not send the document to the printer - outputs the forecast directly to stdout
+-h, --help		shows this help page
 
 ## Example ## 
 Example document: 
--- print_weather.sh
+++ print_weather.sh
@@ -4,12 +4,13 @@ if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]
 then 
 	echo "\
 print_weather.sh - prints current weather forecast to the default printer using lpr. 
-Usage: ./print_weather.sh [-t] [<location>]
+Usage: ./print_weather.sh [-h] [-t] [<location>]
 
 if <location> is omitted, it is determined from the IP address. 
 <location> can be any description of the desired location: City name, coordinates, place name. 
 
--t, --terminal		does not send the document to the printer - outputs the forecast directly to stdout"
+-t, --terminal		does not send the document to the printer - outputs the forecast directly to stdout
+-h, --help		shows this help page"
 	exit 0
 fi 
 
